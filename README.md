d049db0f085679bfd1f8469573b4943fa9010c56
ECHEBII <emmanuelchebii937@gmail.com>
2025-04-12 16:14:17 +0300

    Initial commit for HUDUMATECH

---
+++ README.md
@@ -0,0 +1,23 @@
+# Si-Tukang
+ordering handyman services via the website
+
+[Click Me!👋](https://HUDUMATECH.herokuapp.com/)
+
+## How To Use
+* Installing all package inside devDependencies and dependencies on package.json
+
+   ```sh
+   npm install
+   ```
+* Run the project in Local Server
+
+   ```sh
+   npm start
+   ```
+* Create atleast one account
+
+   ```sh
+   sign up
+   ```
+
+[Document](https://drive.google.com/drive/folders/1UqCgTCnuZ6hJ_xvNElCU6cofMAOv1ixm?usp=sharing)
---
+++ config/config.js
@@ -0,0 +1,34 @@
+const {
+  DB_USER = "root",
+  DB_PASSWORD = "",
+  DB_NAME = "HUDUMATECH_db",
+  DB_HOST = "localhost",
+  DB_PORT = "3306",
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "mysql",
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "mysql",
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "mysql",
+  },
+};
